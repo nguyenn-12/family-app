78a7d73c2837133ad9c018278c2b7b85d8b026ae
QuynhMai273na <nguyenquynhmai273na@gmail.com>
2025-05-01 21:16:22 +0700

    add notification on status bar

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -1,95 +1,3 @@
-// import 'package:family/pages/bottomnav.dart';
-// import 'package:family/pages/calendar.dart';
-// import 'package:family/pages/chat.dart';
-// import 'package:family/pages/gallery.dart';
-// import 'package:family/pages/profile.dart';
-// import 'package:flutter/material.dart';
-// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-// import 'package:family/models/users.dart';
-// import 'package:family/pages/edit_profile.dart';
-//
-//
-// class MainScreen extends StatefulWidget {
-//   const MainScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   State<MainScreen> createState() => _MainScreenState();
-// }
-//
-// class _MainScreenState extends State<MainScreen> {
-//   int _currentIndex = 3;
-//
-//
-//   late final List<Widget> _pages;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _pages = [
-//       CalendarPage(),
-//       GalleryPage(),
-//       ChatPage(
-//                 onGoToProfile: () {
-//           setState(() {
-//             _currentIndex = 3; // hoặc tab Profile
-//           });
-//         },),
-//       ProfilePage(),
-//       EditProfilePage()
-//     ];
-//   }
-//
-//   final List<String> _titles = [
-//     'Calendar ',
-//     'Gallery',
-//     'Chatting Room',
-//     'Profile',
-//     'Edit Profile'
-//   ];
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       appBar: _currentIndex == 3
-//           ? null
-//           : PreferredSize(
-//         preferredSize: const Size.fromHeight(50),
-//         child: AppBar(
-//           centerTitle: true,
-//           title: Text(
-//             _titles[_currentIndex],
-//             style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-//           ),
-//           //backgroundColor: const Color(0xFFA580D8),
-//           flexibleSpace: Container(
-//             decoration: BoxDecoration(
-//               gradient: LinearGradient(
-//                 colors: [
-//                   Color(0xFF00C6A2),
-//                   Color(0xFF007B8F),
-//                 ],
-//                 begin: Alignment.topLeft,
-//                 end: Alignment.bottomRight,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//
-//       body: _pages[_currentIndex],
-//       bottomNavigationBar: BottomNav(
-//         currentIndex: _currentIndex,
-//         onTap: (index) {
-//           setState(() {
-//             _currentIndex = index;
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
 
 // 📄 File: main_screen.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -105,6 +13,8 @@ import 'package:family/pages/edit_profile.dart';
 import 'package:provider/provider.dart';
 import 'package:family/providers/user_provider.dart';
 import 'package:family/providers/unread_provider.dart';
+import 'package:family/services/listener_service.dart';
+
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -123,6 +33,7 @@ class _MainScreenState extends State<MainScreen> {
     final user = Provider.of<UserProvider>(context, listen: false).user;
     if (user != null && user.familyCode.isNotEmpty) {
       _subscribeUnreadCounter(user);
+      setupNotificationListeners(user);
     }
 
     _pages = [
